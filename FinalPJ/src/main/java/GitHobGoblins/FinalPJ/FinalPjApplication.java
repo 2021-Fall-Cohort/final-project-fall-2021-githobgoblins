554eb3ddc04e315652eb59cd6d80dbd3d90b9759
OctGiovanni <90348804+OctGiovanni@users.noreply.github.com>
2021-11-19 14:46:33 -0500

    Add files via upload
    Initial Folder Set Up

---
+++ FinalPJ/src/main/java/GitHobGoblins/FinalPJ/FinalPjApplication.java
@@ -0,0 +1,13 @@
+package GitHobGoblins.FinalPJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalPjApplication.class, args);
+	}
+
+}
---
+++ FinalPJ/src/test/java/GitHobGoblins/FinalPJ/FinalPjApplicationTests.java
@@ -0,0 +1,13 @@
+package GitHobGoblins.FinalPJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalPjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
