70e15c48c9a8aba020e8126433f7cd23b96773a7
jgard2019 <jwgardner22@gmail.com>
2021-11-22 12:00:56 -0500

    built most pojos

---
+++ FinalPJ/src/main/java/GitHobGoblins/FinalPJ/model/Ability.java
@@ -0,0 +1,36 @@
+package GitHobGoblins.FinalPJ.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Ability {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String description;
+
+    public Ability(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Ability(){
+        //zero argument constructor
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
