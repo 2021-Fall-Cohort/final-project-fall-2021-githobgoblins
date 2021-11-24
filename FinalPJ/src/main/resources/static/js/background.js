7a937376e133efa32345f73ac637d743639f0b28
RicoMclellan <ricodmclellan@gmail.com>
2021-11-24 12:29:07 -0500

    wednesday morning frontend

---
+++ FinalPJ/src/main/resources/static/js/background.js
@@ -0,0 +1,16 @@
+import {clearChildren} from "./app.js";
+import {displayHeader} from "./app.js";
+import {displayFooter} from "./app.js";
+import {displayDNDClassView} from "./race.js";
+
+function displayBackgroundView{
+ 
+const acolyteEl = document.createElement("div")
+acolyteEl.classList.add("learnDropdown");
+
+const charlatanEl = document.createElement("div")
+
+
+
+}
+
