56687f2bee6cc62ef127d88d2dfdb57bdab47e45
carsonDentinger <carson.dentinger@gmail.com>
2021-11-24 13:15:36 -0500

    pushing work from this Wed morning

--- FinalPJ/src/main/resources/static/js/background.js
+++ FinalPJ/src/main/resources/static/js/background.js
@@ -4,13 +4,99 @@ import {displayFooter} from "./app.js";
 import {displayDNDClassView} from "./race.js";
 
 function displayBackgroundView{
- 
-const acolyteEl = document.createElement("div")
-acolyteEl.classList.add("learnDropdown");
-
-const charlatanEl = document.createElement("div")
 
 
+    displayHeader(mainContainerEl);
+
+    //making all html elements for the pageTopDiv
+    const backgroundTitleEl = document.createElement("h1");
+    backgroundTitleEl.classList.add("pageTitle");
+    backgroundTitleEl.innerText = "Select Your Background";
+
+    const pageTopDivEl = document.createElement("div");
+    pageTopDivEl.classList.add("pageTopDiv");
+
+    const backgroundFormButtonsDivEl = document.createElement("div");
+    backgroundFormButtonsDivEl.classList.add("formButtons");
+
+    const backgroundFormEl = document.createElement("form");
+
+    const backgroundSelectEl = document.createElement("select");
+    backgroundSelectEl.classList.add("selector");
+
+
+
+
+
+
+
+    const acolyteOptionEl = document.createElement("option");
+    acolyteOptionEl.setAttribute('value', 'acolyte');
+    acolyteOptionEl.innerText = "Acolyte";
+
+    const charlatanOptionEl = document.createElement("option");
+    charlatanOptionEl.setAttribute('value', 'carlatan');
+    charlatanOptionEl.innerText = "Charlatan";
+
+    const criminalOptionEl = document.createElement("option");
+    criminalOptionEl.setAttribute('value', 'criminal');
+    charlatanOptionEl.innerText = "Criminal";
+
+    const entertainerOptionEl = document.createElement("option");
+    entertainerOptionEl.setAttribute('value', 'entertainer');
+    entertainerOptionEl.innerText = "Entertainer";
+
+    const folkHeroOptionEl = document.createElement("option");
+    folkHeroOptionEl.setAttribute('value', 'folkHero');
+    folkHeroOptionEl.innerText = "Folk Hero";
+
+    const guildArtisanOptionEl = document.createElement("option");
+    guildArtisanOptionEl.setAttribute('value', 'guildArtisan');
+    guildArtisanOptionEl.innerText = "Guild Artisan";
+
+    const hermitOptionEl = document.createElement("option");
+    hermitOptionEl.setAttribute('value', 'hemit');
+    hermitOptionEl.innerText = "Hermit";
+
+    const nobleOptionEl = document.createElement("option");
+    nobleOptionEl.setAttribute('value', 'noble');
+    nobleOptionEl.innerText = "Noble";
+
+    const outlanderOptionEl = document.createElement("option");
+    outlanderOptionEl.setAttribute('value', 'outlander');
+    outlanderOptionEl.innerText = "Outlander";
+
+    const sageOptionEl = document.createElement("option");
+    sageOptionEl.setAttribute('value', 'sage');
+    sageOptionEl.innerText = "Sage";
+
+
+    const sailorOptionEl = document.createElement("option");
+    sailorOptionEl.setAttribute('value', 'sailor');
+    sailorOptionEl.innerText = "Sailor";
+
+    const soilderOptionEl = document.createElement("option");
+    sageOptionEl.setAttribute('value', 'soilder');
+    soilderOptionEl.innerText = "Soilder";
+
+    const urchinOptionEl = document.createElement("option");
+    urchinOptionEl.setAttribute('value', 'urchin');
+    urchinOptionEl.innerText = "Urchin";
+
+    //appending all pageTopDiv elements
+
+    backgroundSelectEl.append(acolyteOptionEl);
+    backgroundSelectEl.append(charlatanOptionEl);
+    backgroundSelectEl.append(criminalOptionEl);
+    backgroundSelectEl.append(entertainerOptionEl);
+    backgroundSelectEl.append(folkHeroOptionEl);
+    backgroundSelectEl.append(guildArtisanOptionEl);
+    backgroundSelectEl.append(hermitOptionEl);
+    backgroundSelectEl.append(nobleOptionEl);
+    backgroundSelectEl.append(outlanderOptionEl);
+    backgroundSelectEl.append(sageOptionEl);
+    backgroundSelectEl.append(sailorOptionEl);
+    backgroundSelectEl.append(soilderOptionEl);
+    backgroundSelectEl.append(urchinOptionEl);
 
 }
-
--- FinalPJ/src/main/resources/static/js/race.js
+++ FinalPJ/src/main/resources/static/js/race.js
@@ -1,20 +1,133 @@
 import {clearChildren} from "./app.js";
 import {displayHeader} from "./app.js";
 import {displayFooter} from "./app.js";
-import {displayDNDClassView} from "./race.js";
+import {displayDNDClassView} from "./dndClass.js";
 
 function displayRaceView(mainContainerEl){
 
     displayHeader(mainContainerEl);
 
+    const mainContentDiv = document.createElement("div");
+    mainContentDiv.classList.add("mainContentDiv");
+
     //making all html elements for the pageTopDiv
     const raceTitleEl = document.createElement("h1");
     raceTitleEl.classList.add("pageTitle");
+    raceTitleEl.innerText = "Select Your Race";
 
     const pageTopDivEl = document.createElement("div");
     pageTopDivEl.classList.add("pageTopDiv");
 
-    const 
+    const raceFormButtonsDivEl = document.createElement("div");
+    raceFormButtonsDivEl.classList.add("formButtons");
+    const raceFormEl = document.createElement("form");
+    
+    const raceSelectEl = document.createElement("select");
+    raceSelectEl.classList.add("selector");
+
+    const dragonbornOptionEl = document.createElement("option");
+    elfOptionEl.setAttribute('value', 'dragonborn');
+    dragonbornOptionEl.innerText = "Dragonborn";
+
+    const dwarfOptionEl = document.createElement("option");
+    dwarfOptionEl.setAttribute('value', 'dwarf');
+    dwarfOptionEl.innerText = "Dwarf";
+
+    const elfOptionEl = document.createElement("option");
+    elfOptionEl.setAttribute('value', 'elf');
+    elfOptionEl.innerText = "Elf";
+
+    const gnomeOptionEl = document.createElement("option");
+    gnomeOptionEl.setAttribute('value', 'gnome');
+    gnomeOptionEl.innerText = "Gnome";
+
+    const halfElfOptionEl = document.createElement("option");
+    halfElflfOptionEl.setAttribute('value', 'halfElf');
+    halfElfOptionEl.innerText = "Half-Elf";
+
+    const halfOrcOptionEl = document.createElement("option");
+    halfOrclfOptionEl.setAttribute('value', 'halfOrc');
+    halfOrcOptionEl.innerText = "Half-Orc";
+
+    const halflingOptionEl = document.createElement("option");
+    halflingOptionEl.setAttribute('value', 'halfling');
+    halflingOptionEl.innerText = "Halfling";
+
+    const humanOptionEl = document.createElement("option");
+    humanOptionEl.setAttribute('value', 'human');
+    humanOptionEl.innerText = "Human";
+
+    const tieflingOptionEl = document.createElement("option");
+    halfElflfOptionEl.setAttribute('value', 'tiefling');
+    halfElfOptionEl.innerText = "Tiefling";
+
+    const raceModalButtonEl = document.createElement("button");
+    raceModalButtonEl.setAttribute('id', 'openRaceModal');
+    raceModalButtonEl.innerText = "Learn More About Race";
+
+    const raceImgEl = document.createElement("img");
+    raceImgEl.src = "/FinalPJ/src/main/resources/static/images/queen.jpg";
+    raceImgEl.classList.add("selectorArt");
+
+    //appending all pageTopDiv elements
+
+    raceSelectEl.append(dragonbornOptionEl);
+    raceSelectEl.append(dwarfOptionEl);
+    raceSelectEl.append(elfOptionEl);
+    raceSelectEl.append(gnomeOptionEl);
+    raceSelectEl.append(halfElfOptionEl);
+    raceSelectEl.append(halfOrcOptionEl);
+    raceSelectEl.append(halflingOptionEl);
+    raceSelectEl.append(humanOptionEl);
+    raceSelectEl.append(tieflingOptionEl);
+
+    raceFormEl.append("raceSelectEl");
+
+    raceFormButtonsDivEl.append(raceFormEl);
+    raceFormButtonsDivEl.append(raceModalButtonEl);
+
+    pageTopDivEl.append(raceFormButtonsDivEl);
+    pageTopDivEl.append(raceImgEl);
+
+    mainContentDiv.append(raceTitleEl);
+    mainContentDiv.append(pageTopDivEl);
+
+    //creating all modal elements
+
+    const raceModalDivEl = document.createElement("div");
+    raceModalDivEl.classList.add("modal");
+    raceModalDivEl.setAttribute('id', 'raceModal');
+
+    const raceModalContentDivEl = document.createElement("div");
+    raceModalContentDivEl.classList.add("modalContent");
+
+    const modalHeaderDivEl = document.createElement("div");
+    modalHeaderDivEl.classList.add("modalHeader");
+    
+    const modalCloseSpanEl = document.createElement("span");
+    modalCloseSpanEl.classList.add("close");
+    modalCloseSpanEl.innerHTML = "&times;";
+
+    const raceModalHeaderEl = document.createElement("h2");
+    raceModalHeaderEl.classList.add("raceModalHeader");
+    raceModalHeaderEl.innerText = "Race";
+
+    const raceModalBodyDivEl = document.createElement("div");
+    raceModalBodyDivEl.classList.add("modalBody");
+
+    const raceModalBodyContentEl = document.createElement("p");
+    raceModalBodyContentEl.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eaque quia adipisci quis alias, laborum quibusdam dignissimos veniam incidunt, tempore, ex eius repellendus. Iure repudiandae quisquam itaque quis neque quam!";
+
+    //appending all modal content
+
+    modalHeaderDivEl.append(modalCloseSpanEl);
+    modalHeaderDivEl.append(raceModalHeaderEl);
+
+    raceModalBodyDivEl.append(raceModalBodyContentEl);
+
+    raceModalContentDivEl.append();
+    raceModalContentDivEl.append();
+
 
     displayFooter(mainContainerEl);
 }
