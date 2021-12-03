8069b9496693e5daf144c27f30b0de49e7dbe880
carsonDentinger <carson.dentinger@gmail.com>
2021-12-03 16:20:59 -0500

    moved folders

---
+++ src/main/resources/static/js/features.js
@@ -0,0 +1,217 @@
+import {clearChildren} from "./app.js";
+import {displayDNDClassView} from "./dndClass.js";
+import { displayHeader } from "./app.js";
+import { displayFooter } from "./app.js";
+
+function displayFeaturesView(mainContainerEl) {
+
+    displayHeader(mainContainerEl);
+
+    //making all fields and content for features page
+
+    const mainContentDivEl = document.createElement("div");
+    mainContentDivEl.classList.add("mainContentDiv");
+
+    const featureHeaderEl = document.createElement("h1");
+    featureHeaderEl.classList.add("featureHeader");
+    featureHeaderEl.innerText = "Basic Info";
+
+    const featureDivEl = document.createElement("div");
+    featureDivEl.classList.add("featureDiv");
+
+    const nameLabelEl = document.createElement("h3");
+    nameLabelEl.innerText = "Name";
+
+    const nameFieldEl = document.createElement("input");
+    nameFieldEl.type = "text";
+    nameFieldEl.placeholder = "Enter Your Character's Name";
+
+    const levelLabelEl = document.createElement("h3");
+    levelLabelEl.innerText = "Level";
+
+    const levelFieldEl = document.createElement("input");
+    levelFieldEl.type = "number";
+    levelFieldEl.placeholder = "Choose 1-3";
+
+    const alignmentLabelEl = document.createElement("h3");
+    alignmentLabelEl.innerText = "Alignment";
+
+    const alignmentFieldEl = document.createElement("input");
+    alignmentFieldEl.type = "text";
+    alignmentFieldEl.placeholder = "Select an Alignment";
+
+    //making level modal elements
+
+    const levelModalButtonEl = document.createElement("button");
+    levelModalButtonEl.innerText = "?"
+
+    const levelModalDivEl = document.createElement("div");
+    levelModalDivEl.classList.add("modal");
+    levelModalDivEl.setAttribute('id', 'levelModalDiv');
+
+    const levelModalContentDivEl = document.createElement("div");
+    levelModalContentDivEl.classList.add("levelModalContent");
+
+    const levelModalHeaderDivEl = document.createElement("div");
+    levelModalHeaderDivEl.classList.add("levelModalHeaderDiv");
+
+    const levelModalCloseSpanEl = document.createElement("span");
+    levelModalCloseSpanEl.classList.add("close");
+    levelModalCloseSpanEl.innerHTML = "&times;";
+
+    const levelModalHeaderEl = document.createElement("h2");
+    levelModalHeaderEl.classList.add("levelModalHeader");
+    levelModalHeaderEl.innerText = "Level";
+
+    const levelModalBodyDivEl = document.createElement("div");
+    levelModalBodyDivEl.classList.add("modalBody");
+
+    const levelModalBodyContentEl = document.createElement("p");
+    levelModalBodyContentEl.innerText = "As your character goes on adventures and overcomes challenges, he or she gains experience, represented by experience points. A character who reaches a specified experience point total advances in capability. This advancement is called gaining a level. When your character gains a level, his or her class often grants additional features, as detailed in the class description. Some of these features allow you to increase your ability scores, either increasing two scores by 1 each or increasing one score by 2. You can’t increase an ability score above 20. In addition, every character's proficiency bonus increases at certain levels.";
+
+    //appending level modal elements
+
+    levelModalHeaderDivEl.append(levelModalCloseSpanEl);
+    levelModalHeaderDivEl.append(levelModalHeaderEl);
+
+    levelModalBodyDivEl.append(levelModalBodyContentEl);
+
+    levelModalContentDivEl.append(levelModalHeaderDivEl);
+    levelModalContentDivEl.append(levelModalBodyDivEl);
+
+    levelModalDivEl.append(levelModalContentDivEl);
+
+    //making level modal work
+
+    levelModalButtonEl.addEventListener("click", ()=> {  
+        levelModalDivEl.style.display = "block";
+    })
+  
+    levelModalCloseSpanEl.onclick = function() {
+        levelModalDivEl.style.display = "none";
+    }
+  
+    window.onclick = function(event) {
+        if (event.target == levelModalDivEl) {
+            levelModalDivEl.style.display = "none";
+        }
+    }
+
+    //making all alignment modal elements
+
+    const alignmentModalButtonEl = document.createElement("button");
+    alignmentModalButtonEl.innerText = "?"
+
+    const alignmentModalDivEl = document.createElement("div");
+    alignmentModalDivEl.classList.add("modal");
+    alignmentModalDivEl.setAttribute('id', 'levelModalDiv');
+
+    const alignmentModalContentDivEl = document.createElement("div");
+    alignmentModalContentDivEl.classList.add("alignmentModalContent");
+
+    const alignmentModalHeaderDivEl = document.createElement("div");
+    alignmentModalHeaderDivEl.classList.add("alignmentModalHeaderDiv");
+
+    const alignmentModalCloseSpanEl = document.createElement("span");
+    alignmentModalCloseSpanEl.classList.add("close");
+    alignmentModalCloseSpanEl.innerHTML = "&times;";
+
+    const alignmentModalHeaderEl = document.createElement("h2");
+    alignmentModalHeaderEl.classList.add("alignmentModalHeader");
+    alignmentModalHeaderEl.innerText = "Alignment";
+
+    const alignmentModalBodyDivEl = document.createElement("div");
+    alignmentModalBodyDivEl.classList.add("modalBody");
+
+    const alignmentModalBodyContentEl = document.createElement("p");
+    alignmentModalBodyContentEl.innerText = "A typical creature in the game world has an alignment, which broadly describes its moral and personal attitudes. Alignment is a combination of two factors: one identifies morality (good, evil, or neutral), and the other describes attitudes toward society and order (lawful, chaotic, or neutral). Thus, nine distinct alignments define the possible combinations. These brief summaries of the nine alignments describe the typical behavior of a creature with that alignment. Individuals might vary significantly from that typical behavior, and few people are perfectly and consistently faithful to the precepts of their alignment.";
+
+    //appending all alignment modal content
+
+    alignmentModalHeaderDivEl.append(alignmentModalCloseSpanEl);
+    alignmentModalHeaderDivEl.append(alignmentModalHeaderEl);
+
+    alignmentModalBodyDivEl.append(alignmentModalBodyContentEl);
+
+    alignmentModalContentDivEl.append(alignmentModalHeaderDivEl);
+    alignmentModalContentDivEl.append(alignmentModalBodyDivEl);
+
+    alignmentModalDivEl.append(alignmentModalContentDivEl);
+
+    //making alignment modal work
+
+    alignmentModalButtonEl.addEventListener("click", ()=> {  
+        alignmentModalDivEl.style.display = "block";
+    })
+  
+    alignmentModalCloseSpanEl.onclick = function() {
+        alignmentModalDivEl.style.display = "none";
+    }
+  
+    window.onclick = function(event) {
+        if (event.target == alignmentModalDivEl) {
+            alignmentModalDivEl.style.display = "none";
+        }
+    }
+
+    
+
+    const forwardButtonEl = document.createElement("button");
+    forwardButtonEl.classList.add("navButtons");
+    forwardButtonEl.setAttribute('id', 'backgroundForwardButton');
+    forwardButtonEl.innerText = ">";
+
+  
+
+    let currentCharacter; 
+
+    //wiring up forward button
+    forwardButtonEl.addEventListener("click", () => {
+        const characterBaseJson = {
+            "name": nameFieldEl.value,
+            "level": levelFieldEl.value,
+            "alignment": alignmentFieldEl.value,
+            "proficiencyBonus": 2,
+            "experiencePoints": 0
+        };
+          
+        fetch(`http://localhost:8080/buildcharacter/base/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(characterBaseJson)
+      })
+    .then(res => res.json())
+    .then(character => {
+        clearChildren(mainContainerEl);
+        displayDNDClassView(mainContainerEl, character);
+        currentCharacter = character;
+    })
+    .catch(err => console.error(err));
+    })
+
+    //appending all features page content
+    
+    featureDivEl.append(nameLabelEl);
+    featureDivEl.append(nameFieldEl);
+    featureDivEl.append(levelLabelEl);
+    featureDivEl.append(levelFieldEl);
+    featureDivEl.append(levelModalButtonEl);
+    featureDivEl.append(alignmentLabelEl);
+    featureDivEl.append(alignmentFieldEl);
+    featureDivEl.append(alignmentModalButtonEl);
+    featureDivEl.append(forwardButtonEl);
+
+    mainContentDivEl.append(alignmentModalDivEl);
+    mainContentDivEl.append(levelModalDivEl);
+    mainContentDivEl.append(featureHeaderEl);
+    mainContentDivEl.append(featureDivEl);
+
+    mainContainerEl.append(mainContentDivEl);
+
+    displayFooter(mainContainerEl);
+
+}
+
+export {displayFeaturesView}
---
+++ src/main/resources/static/js/race.js
@@ -0,0 +1,326 @@
+import {clearChildren} from "./app.js";
+import {displayHeader} from "./app.js";
+import {displayFooter} from "./app.js";
+import { displayBackgroundView } from "./background.js";
+import { displayDNDClassView } from "./dndClass.js";
+
+
+function displayRaceView(mainContainerEl, currentCharacter){
+
+  displayHeader(mainContainerEl);
+
+  const mainContentDiv = document.createElement("div");
+  mainContentDiv.classList.add("mainContentDiv");
+
+  //making all html elements for the pageTopDiv
+  const raceTitleEl = document.createElement("h1");
+  raceTitleEl.classList.add("pageTitle");
+  raceTitleEl.innerText = "Select Your Race";
+
+  const pageTopDivEl = document.createElement("div");
+  pageTopDivEl.classList.add("pageTopDiv");
+
+  const raceFormButtonsDivEl = document.createElement("div");
+  raceFormButtonsDivEl.classList.add("formButtons");
+  const raceFormEl = document.createElement("form");
+    
+  const raceSelectEl = document.createElement("select");
+  raceSelectEl.classList.add("selector");
+
+  const dragonbornOptionEl = document.createElement("option");
+  dragonbornOptionEl.setAttribute('value', 'dragonborn');
+  dragonbornOptionEl.innerText = "Dragonborn";
+
+  const dwarfOptionEl = document.createElement("option");
+  dwarfOptionEl.setAttribute('value', 'dwarf');
+  dwarfOptionEl.innerText = "Dwarf";
+
+  const elfOptionEl = document.createElement("option");
+  elfOptionEl.setAttribute('value', 'elf');
+  elfOptionEl.innerText = "Elf";
+
+  const gnomeOptionEl = document.createElement("option");
+  gnomeOptionEl.setAttribute('value', 'gnome');
+  gnomeOptionEl.innerText = "Gnome";
+
+  const halfElfOptionEl = document.createElement("option");
+  halfElfOptionEl.setAttribute('value', 'halfElf');
+  halfElfOptionEl.innerText = "Half-Elf";
+
+  const halfOrcOptionEl = document.createElement("option");
+  halfOrcOptionEl.setAttribute('value', 'halfOrc');
+  halfOrcOptionEl.innerText = "Half-Orc";
+
+  const halflingOptionEl = document.createElement("option");
+  halflingOptionEl.setAttribute('value', 'halfling');
+  halflingOptionEl.innerText = "Halfling";
+
+  const humanOptionEl = document.createElement("option");
+  humanOptionEl.setAttribute('value', 'human');
+  humanOptionEl.innerText = "Human";
+
+  const tieflingOptionEl = document.createElement("option");
+  tieflingOptionEl.setAttribute('value', 'tiefling');
+  tieflingOptionEl.innerText = "Tiefling";
+
+  //this is the open modal button
+  const raceModalButtonEl = document.createElement("button");
+  raceModalButtonEl.setAttribute('id', 'openRaceModal');
+  raceModalButtonEl.innerText = "Learn More About Race";
+  console.log(raceModalButtonEl);
+
+  // creating nav buttons
+  const backButtonEl = document.createElement("button");
+  backButtonEl.classList.add("navButtons");
+  backButtonEl.setAttribute('id', 'raceBackButton');
+  backButtonEl.innerText = "<";
+
+  const forwardButtonEl = document.createElement("button");
+  forwardButtonEl.classList.add("navButtons");
+  forwardButtonEl.setAttribute('id', 'raceForwardButton');
+  forwardButtonEl.innerText = ">";
+
+  const raceImgEl = document.createElement("img");
+  raceImgEl.src = "./images/elf.jpg";
+  raceImgEl.classList.add("selectorArt");
+
+  //appending all pageTopDiv elements
+
+  raceSelectEl.append(dragonbornOptionEl);
+  raceSelectEl.append(dwarfOptionEl);
+  raceSelectEl.append(elfOptionEl);
+  raceSelectEl.append(gnomeOptionEl);
+  raceSelectEl.append(halfElfOptionEl);
+  raceSelectEl.append(halfOrcOptionEl);
+  raceSelectEl.append(halflingOptionEl);
+  raceSelectEl.append(humanOptionEl);
+  raceSelectEl.append(tieflingOptionEl);
+
+  raceFormEl.append(raceSelectEl);
+
+  raceFormButtonsDivEl.append(raceFormEl);
+  raceFormButtonsDivEl.append(raceModalButtonEl);
+
+  pageTopDivEl.append(backButtonEl);
+  pageTopDivEl.append(raceFormButtonsDivEl);
+  pageTopDivEl.append(raceImgEl);
+  pageTopDivEl.append(forwardButtonEl);
+
+  mainContentDiv.append(raceTitleEl);
+  mainContentDiv.append(pageTopDivEl);
+
+  //creating all modal elements
+
+  const raceModalDivEl = document.createElement("div");
+  raceModalDivEl.classList.add("modal");
+  raceModalDivEl.setAttribute('id', 'raceModalDiv');
+
+  const raceModalContentDivEl = document.createElement("div");
+  raceModalContentDivEl.classList.add("raceModalContent");
+
+  const modalHeaderDivEl = document.createElement("div");
+  modalHeaderDivEl.classList.add("raceModalHeader");
+    
+  const raceModalCloseSpanEl = document.createElement("span");
+  raceModalCloseSpanEl.classList.add("raceClose");
+  raceModalCloseSpanEl.innerHTML = "&times;";
+
+  const raceModalHeaderEl = document.createElement("h2");
+  raceModalHeaderEl.classList.add("raceModalHeader");
+  raceModalHeaderEl.innerText = "Race";
+
+  const raceModalBodyDivEl = document.createElement("div");
+  raceModalBodyDivEl.classList.add("raceModalBody");
+
+  const raceModalBodyContentEl = document.createElement("p");
+  raceModalBodyContentEl.innerText = "Humans are the most common people in the worlds of D&D, but they live and work alongside dwarves, elves, halflings, and countless other fantastic species. Your character belongs to one of these peoples. Not every intelligent race of the multiverse is appropriate for a player-controlled adventurer. Dwarves, elves, halflings, and humans are the most common races to produce the sort of adventurers who make up typical parties. Dragonborn, gnomes, half-elves, half-orcs, and tieflings are less common as adventurers. Your choice of race affects many different aspects of your character. It establishes fundamental qualities that exist throughout your character’s adventuring career. When making this decision, keep in mind the kind of character you want to play. For example, a halfling could be a good choice for a sneaky rogue, a dwarf makes a tough warrior, and an elf can be a master of arcane magic. Your character race not only affects your ability scores and traits but also provides the cues for building your character’s story. Each race’s description in this section includes information to help you roleplay a character of that race, including personality, physical appearance, features of society, and racial alignment tendencies. These details are suggestions to help you think about your character; adventurers can deviate widely from the norm for their race. It’s worthwhile to consider why your character is different, as a helpful way to think about your character’s background and personality";
+
+  //appending all modal content
+
+  modalHeaderDivEl.append(raceModalCloseSpanEl);
+  modalHeaderDivEl.append(raceModalHeaderEl);
+
+  raceModalBodyDivEl.append(raceModalBodyContentEl);
+
+  raceModalContentDivEl.append(modalHeaderDivEl);
+  raceModalContentDivEl.append(raceModalBodyDivEl);
+
+  raceModalDivEl.append(raceModalContentDivEl);
+
+  mainContentDiv.append(raceModalDivEl);
+
+  //making modal work
+
+  raceModalButtonEl.addEventListener("click", ()=> {
+    raceModalDivEl.style.display = "block";
+  })
+
+  raceModalCloseSpanEl.onclick = function() {
+    raceModalDivEl.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == raceModalDivEl) {
+      raceModalDivEl.style.display = "none";
+    }
+  }
+
+    //creating all pageBottomDiv elements
+
+    const pageBottomDivEl = document.createElement("div");
+    pageBottomDivEl.classList.add("div");
+
+    const raceLearnDropdownDivEl = document.createElement("div");
+    raceLearnDropdownDivEl.classList.add("learnDropdown");
+
+    const raceDropdownLabelEl = document.createElement("label");
+    raceDropdownLabelEl.setAttribute('for', 'raceNames');
+    raceDropdownLabelEl.setAttribute('id', 'raceLearnLabel');
+
+    const raceLearnFormEl = document.createElement("form");
+    
+    const raceLearnSelectEl = document.createElement("select");
+    raceLearnSelectEl.setAttribute('name', 'raceNames');
+    raceLearnSelectEl.setAttribute('id', 'raceNames');
+
+    const dragonbornLearnOptionEl = document.createElement("option");
+    dragonbornLearnOptionEl.setAttribute('value', 'dragonborn');
+    dragonbornLearnOptionEl.innerText = "Dragonborn";
+
+    const dwarfLearnOptionEl = document.createElement("option");
+    dwarfLearnOptionEl.setAttribute('value', 'dwarf');
+    dwarfLearnOptionEl.innerText = "Dwarf";
+
+    const elfLearnOptionEl = document.createElement("option");
+    elfLearnOptionEl.setAttribute('value', 'elf');
+    elfLearnOptionEl.innerText = "Elf";
+
+    const gnomeLearnOptionEl = document.createElement("option");
+    gnomeLearnOptionEl.setAttribute('value', 'gnome');
+    gnomeLearnOptionEl.innerText = "Gnome";
+
+    const halfElfLearnOptionEl = document.createElement("option");
+    halfElfLearnOptionEl.setAttribute('value', 'halfElf');
+    halfElfLearnOptionEl.innerText = "Half-Elf";
+
+    const halfOrcLearnOptionEl = document.createElement("option");
+    halfOrcLearnOptionEl.setAttribute('value', 'halfOrc');
+    halfOrcLearnOptionEl.innerText = "Half-Orc";
+
+    const halflingLearnOptionEl = document.createElement("option");
+    halflingLearnOptionEl.setAttribute('value', 'halfling');
+    halflingLearnOptionEl.innerText = "Halfling";
+
+    const humanLearnOptionEl = document.createElement("option");
+    humanLearnOptionEl.setAttribute('value', 'human');
+    humanLearnOptionEl.innerText = "Human";
+
+    const tieflingLearnOptionEl = document.createElement("option");
+    tieflingLearnOptionEl.setAttribute('value', 'tiefling');
+    tieflingLearnOptionEl.innerText = "Tiefling";
+
+    const raceMoreInfoButtonEl = document.createElement("button");
+    raceMoreInfoButtonEl.innerText = "More Info";
+    raceMoreInfoButtonEl.setAttribute('id', 'raceMoreInfoButton');
+
+    //creating more info elements for page dropdown
+
+    const raceMoreInfoDivEl = document.createElement("div");
+    raceMoreInfoDivEl.setAttribute('id', 'raceMoreInfoDiv');
+
+    const raceMoreInfoTextEl = document.createElement("p");
+    raceMoreInfoTextEl.classList.add("raceMoreInfoText");
+    raceMoreInfoTextEl.innerText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur numquam quaerat accusantium voluptatum vero necessitatibus architecto molestiae molestias eligendi amet facere aspernatur, sed ullam a dolorum atque laborum nulla labore.";
+
+    //appending more info content
+
+    raceMoreInfoDivEl.append(raceMoreInfoTextEl);
+
+    mainContentDiv.append(raceMoreInfoDivEl);
+
+    //appending all pageBottomDiv elements
+
+    raceLearnSelectEl.append(dragonbornLearnOptionEl);
+    raceLearnSelectEl.append(dwarfLearnOptionEl);
+    raceLearnSelectEl.append(elfLearnOptionEl);
+    raceLearnSelectEl.append(gnomeLearnOptionEl);
+    raceLearnSelectEl.append(halfElfLearnOptionEl);
+    raceLearnSelectEl.append(halfOrcLearnOptionEl);
+    raceLearnSelectEl.append(halflingLearnOptionEl);
+    raceLearnSelectEl.append(humanLearnOptionEl);
+    raceLearnSelectEl.append(tieflingLearnOptionEl);
+
+    raceLearnFormEl.append(raceLearnSelectEl);
+
+    raceLearnDropdownDivEl.append(raceDropdownLabelEl);
+    raceLearnDropdownDivEl.append(raceLearnFormEl);
+
+  pageBottomDivEl.append(raceLearnDropdownDivEl);
+  pageBottomDivEl.append(raceMoreInfoButtonEl);
+  mainContentDiv.append(pageBottomDivEl);
+
+  //creating raceLearnMoreDiv elements
+
+  const raceLearnMoreDivEl = document.createElement("div");
+  raceLearnMoreDivEl.setAttribute('id', 'raceLearnMoreDiv');
+  const raceLearnMoreContentEl = document.createElement("p");
+  raceLearnMoreContentEl.setAttribute('id', 'raceLearnMoreContent');
+
+  //appending raceLearnMoreDiv content
+
+  raceLearnMoreDivEl.append(raceLearnMoreContentEl);
+  mainContentDiv.append(raceLearnMoreDivEl);
+
+  //learn more about individual races dropdown 
+
+  function learnRaceFunction(){
+    var x = document.getElementById("raceMoreInfoDiv");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      } 
+      else {
+        x.style.display = "block";
+      }
+  }
+  
+  raceMoreInfoButtonEl.addEventListener("click", ()=> {
+    learnRaceFunction();
+  })
+
+    //wiring up nav buttons
+
+  backButtonEl.addEventListener("click", () => {
+      clearChildren(mainContainerEl);
+      displayDNDClassView(mainContainerEl);
+  });
+
+  forwardButtonEl.addEventListener("click", () => {
+    const raceJson = {
+      "name" : raceSelectEl.value,
+      "description": "warlock boi"
+    };
+      fetch(`http://localhost:8080/buildcharacter/race/${currentCharacter.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(raceJson)
+      })
+    .then(res => res.json())
+    .then(character => {
+        clearChildren(mainContainerEl);
+        displayBackgroundView(mainContainerEl, character);
+        currentCharacter = character;
+    })
+    .catch(err => console.error(err));
+  })
+      
+    
+    
+
+    mainContainerEl.append(mainContentDiv);
+
+    displayFooter(mainContainerEl);
+}
+
+export {displayRaceView}
