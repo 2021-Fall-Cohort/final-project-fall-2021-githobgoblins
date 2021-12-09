e1baaac5615672f43eaf4ec0893d3d2b756039d1
jgard2019 <jwgardner22@gmail.com>
2021-12-08 22:12:18 -0500

    half done

--- src/main/resources/static/js/rulesPage.js
+++ src/main/resources/static/js/rulesPage.js
@@ -1,4 +1,11 @@
-import { classJson, raceJson, backgroundJson } from "./descJson.js";
+import {
+    clearChildren
+} from "./app.js";
+import {
+    classJson,
+    raceJson,
+    backgroundJson
+} from "./descJson.js";
 
 function displayRulesPage(mainContainerEl) {
     const mainSection = document.createElement("section");
@@ -7,151 +14,228 @@ function displayRulesPage(mainContainerEl) {
     mainContainerEl.append(mainSection);
 
     const mainH1El = document.createElement("h1");
-    mainH1El.innerText= "DND Rules Page";
+    mainH1El.innerText = "DND Rules Page";
 
     const contentUL = document.createElement("ul");
     mainSection.append(mainH1El);
     mainSection.appendChild(contentUL);
-
-    //infoBox and infoBoxText
+    
     const infoBox = document.createElement("div");
     infoBox.classList.add("infoBoxDiv");
-    const infoBoxText = document.createElement("p");
-    infoBoxText.classList.add("infoBoxText");
-    infoBoxText.innerText = "";
     const infoBoxH2 = document.createElement("h2");
     infoBoxH2.classList.add("infoBoxH2");
-    infoBoxH2.innerText="Click on what you'd like to learn more about.";
-
+    infoBoxH2.innerText = "Click on what you'd like to learn more about.";
 
     mainSection.appendChild(infoBox);
     infoBox.appendChild(infoBoxH2);
-    infoBox.appendChild(infoBoxText);
-    
-
-    //create and append ListItems as needed
 
     //Class section
     const listItem1 = document.createElement("li");
     listItem1.classList.add("item-list");
     const item1Button = document.createElement("button");
     item1Button.classList.add("clicktohide");
-    item1Button.innerText ="Classes";
+    item1Button.innerText = "Classes";
 
     //Race section
     const listItem2 = document.createElement("li");
     listItem2.classList.add("item-list");
     const item2Button = document.createElement("button");
     item2Button.classList.add("clicktohide");
-    item2Button.innerText= "Races ";
-    
+    item2Button.innerText = "Races ";
+
     //Background section
     const listItem3 = document.createElement("li");
     listItem3.classList.add("item-list");
     const item3Button = document.createElement("button");
     item3Button.classList.add("clicktohide");
-    item3Button.innerText= "Backgrounds";
+    item3Button.innerText = "Backgrounds";
 
     //Ability Scores
     const listItem4 = document.createElement("li");
     listItem4.classList.add("item-list");
     const item4Button = document.createElement("button");
     item4Button.classList.add("clicktohide");
-    item4Button.innerText= "Ability Scores";
+    item4Button.innerText = "Ability Scores";
 
     //Adventuring
     const listItem5 = document.createElement("li");
     listItem5.classList.add("item-list");
     const item5Button = document.createElement("button");
     item5Button.classList.add("clicktohide");
-    item5Button.innerText= "Adventuring";
+    item5Button.innerText = "Adventuring";
 
     //Monsters
     const listItem6 = document.createElement("li");
     listItem6.classList.add("item-list");
     const item6Button = document.createElement("button");
     item6Button.classList.add("clicktohide");
-    item6Button.innerText= "Monsters";
+    item6Button.innerText = "Monsters";
 
     //Resting
     const listItem7 = document.createElement("li");
     listItem7.classList.add("item-list");
     const item7Button = document.createElement("button");
     item7Button.classList.add("clicktohide");
-    item7Button.innerText= "Resting";
+    item7Button.innerText = "Resting";
 
     //Combat
     const listItem8 = document.createElement("li");
     listItem8.classList.add("item-list");
     const item8Button = document.createElement("button");
     item8Button.classList.add("clicktohide");
-    item8Button.innerText= "Combat";
-    
-
+    item8Button.innerText = "Combat";
 
     //classes
     contentUL.appendChild(listItem1);
     listItem1.appendChild(item1Button);
 
-    item1Button.addEventListener("click", ()=>{
+    item1Button.addEventListener("click", () => {
+
+        clearChildren(infoBox);
         let i = 0;
-        infoBoxText.innerText = "";
+        let someDudesFav = document.createElement("hr");
+        infoBox.append(infoBoxH2);
+        infoBox.append(someDudesFav);
+        infoBoxH2.innerText = "Classes";
+
         fetch('https://www.dnd5eapi.co/api/classes', {
-        method: 'GET'
-    })
-        .then(response => response.json())
-        .then(classes => {
-            classes.results.forEach(result=> {
-                let classObjectName = document.createElement("h3");
-                classObjectName.innerText = result.name;
-                infoBoxText.appendChild(classObjectName);
+                method: 'GET'
+            })
+            .then(response => response.json())
+            .then(classes => {
+                classes.results.forEach(result => {
+                    let classObjectName = document.createElement("h2");
+                    classObjectName.classList.add("infoBoxH2");
+                    classObjectName.innerText = result.name;
+                    infoBox.appendChild(classObjectName);
 
-                let classDesc = document.createElement("p");
-                classDesc.classList.add("class-info");
+                    let classDesc = document.createElement("p");
+                    classDesc.classList.add("infoBoxText");
 
-                classDesc.innerText = classJson[i].desc;
+                    classDesc.innerText = classJson[i].desc;
 
-                classObjectName.append(classDesc);
-                i++;
-            });
-        })
+                    infoBox.append(classDesc);
+                    i++;
+                });
+            })
     });
 
     //races
     contentUL.appendChild(listItem2);
     listItem2.appendChild(item2Button);
-    item2Button.addEventListener("click", ()=>{
+    item2Button.addEventListener("click", () => {
+
+        clearChildren(infoBox);
         let i = 0;
-        infoBoxText.innerText = "";
+        let someDudesFav = document.createElement("hr");
+        infoBox.append(infoBoxH2);
+        infoBox.append(someDudesFav);
+        infoBoxH2.innerText = "Races";
+
         fetch('https://www.dnd5eapi.co/api/races', {
-        method: 'GET'
-        })
-        .then(response => response.json())
-        .then(races => {
-            races.results.forEach(result => {
-                let raceObjectName = document.createElement("h3");
-                raceObjectName.innerText = result.name;
-                infoBoxText.appendChild(raceObjectName);
-
-                let raceDesc = document.createElement("p");
-                raceDesc.classList.add("race-info");
-                raceDesc.innerText = classJson[i].desc;
-
-                raceObjectName.append(raceDesc);
-                i++;
+                method: 'GET'
             })
-        })
+            .then(response => response.json())
+            .then(races => {
+                races.results.forEach(result => {
+                    let raceObjectName = document.createElement("h2");
+                    raceObjectName.classList.add("infoBoxH2");
+                    raceObjectName.innerText = result.name;
+                    infoBox.appendChild(raceObjectName);
 
-    })
+                    let raceDesc = document.createElement("p");
+                    raceDesc.classList.add("race-info");
+                    raceDesc.innerText = raceJson[i].desc;
 
+                    infoBox.append(raceDesc);
+                    i++;
+                });
+            });
+    });
 
     //backgrounds
     contentUL.appendChild(listItem3);
     listItem3.appendChild(item3Button);
+    item3Button.addEventListener("click", () => {
+
+        clearChildren(infoBox);
+        let someDudesFav = document.createElement("hr");
+        infoBox.append(infoBoxH2);
+        infoBox.append(someDudesFav);
+        infoBoxH2.innerText = "Backgrounds";
+
+        backgroundJson.forEach(object => {
+            let backgroundObjectName = document.createElement("h2");
+            let backgroundDesc = document.createElement("p");
+            backgroundObjectName.classList.add("infoBoxH2");
+            backgroundObjectName.innerText = object.title;
+            backgroundDesc.classList.add("background-info");
+            backgroundDesc.innerText = object.desc;
+
+            infoBox.appendChild(backgroundObjectName);
+            infoBox.appendChild(backgroundDesc);
+        });
+    });
 
     //Ability Scores
     contentUL.appendChild(listItem4);
     listItem4.appendChild(item4Button);
+    item4Button.addEventListener("click", () => {
+
+        clearChildren(infoBox);
+        let someDudesFav = document.createElement("hr");
+        infoBox.append(infoBoxH2);
+        infoBox.append(someDudesFav);
+        infoBoxH2.innerText = "Ability Scores";
+
+        fetch('https://www.dnd5eapi.co/api/ability-scores', {
+                method: 'GET'
+            })
+            .then(res => res.json())
+            .then(abilities => {
+                abilities.results.forEach(mainAbility => {
+                    let abilityScore = document.createElement("h2");
+                    abilityScore.classList.add("infoBoxH2");
+                    abilityScore.innerText = mainAbility.name;
+                    infoBox.appendChild(abilityScore);
+
+                    let abilityUrl = "" + mainAbility.url;
+                    fetch("https://www.dnd5eapi.co" + abilityUrl, {
+                            method: 'GET'
+                        })
+                        .then(res => res.json())
+                        .then(abilities2 => {
+                            let abilityDeets = document.createElement("p");
+                            abilityDeets.classList.add("ability-desc");
+                            abilityDeets.innerText = abilities2.desc;
+                            infoBox.appendChild(abilityScore);
+                            infoBox.appendChild(abilityDeets);
+                            console.log(abilityUrl);
+
+                            if(abilityScore.innerText == "CHA"){
+                                abilityScore.innerText = abilities2.full_name;
+                            }
+                            if(abilityScore.innerText == "CON"){
+                                abilityScore.innerText = abilities2.full_name;
+                            }
+                            if(abilityScore.innerText == "DEX"){
+                                abilityScore.innerText = abilities2.full_name;
+                            }
+                            if(abilityScore.innerText == "INT"){
+                                abilityScore.innerText = abilities2.full_name;
+                            }
+                            if(abilityScore.innerText == "STR"){
+                                abilityScore.innerText = abilities2.full_name;
+                            }
+                            if(abilityScore.innerText == "WIS"){
+                                abilityScore.innerText = abilities2.full_name;
+                            }
+
+                        })
+                })
+            })
+
+    })
 
     //Adventuring
     contentUL.appendChild(listItem5);
@@ -169,12 +253,8 @@ function displayRulesPage(mainContainerEl) {
     contentUL.appendChild(listItem8);
     listItem8.appendChild(item8Button);
 
-
-
-
-
-
 }
 
-
-export {displayRulesPage};
+export {
+    displayRulesPage
+};
